e77b8635d17520f98e8935bedc4b6132b967d616
Maya Gasiorowski <mayagasiorowski@gmail.com>
2022-10-22 15:09:59 -0400

    Adds fruit stuff

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
+fruits = ['apples', 'oranges', 'bananas', 'tomatoes', 'grapes']
+avocado = []
+
 if __name__ == "__main__":
     print('This is a testing program for github')
+    for fruit in fruits:
+        fruit += ' avocados'
+        avocado.append(fruit)
+
+print(avocado)
